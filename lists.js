d61719ba560733846b4881113469803e8891bc4c
Chad Griffis <cmg123@me.com>
2016-10-25 16:18:10 -0500

    adds definition and some outline of the lists.js file

--- lists.js
+++ lists.js
@@ -1,1 +1,5 @@
-lists.js
+// this is a creation of a simple list class.  
+// ADT = Abstract Data Type
+// problems best solved with lists.
+
+// A list is an ordered sequence of data.  Each data item stored in a list is called an element.  
