570fded16ef8dd9e8bbf623c02b25aa0d1e686cd
Chad Griffis <cmg123@me.com>
2016-11-05 13:50:21 -0500

    adds the list class with append and find methods

--- lists.js
+++ lists.js
@@ -3,3 +3,26 @@
 // problems best solved with lists.
 
 // A list is an ordered sequence of data.  Each data item stored in a list is called an element.  
+
+// list abstract data type (ADT)
+
+class List {
+  constructor() {
+    this.listSize = 0;
+    this.pos = 0;
+    this.dataStore = [];
+  };
+
+  append(element) {
+    this.dataStore[this.listSize++] = element;
+  };
+
+  find(element) {
+    for(const data of this.dataStore) {
+      if (data === element) {
+        return data;
+      }
+    }
+    return -1;
+  };
+}
