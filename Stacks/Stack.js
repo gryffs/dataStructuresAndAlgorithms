f064b5fad8b938fcae8bba05cd8a578f9fa45395
Chad Griffis <cmg123@me.com>
2016-11-08 21:58:40 -0600

    adds the stack class and test

--- Stacks/Stack.js
+++ Stacks/Stack.js
@@ -1,1 +1,8 @@
-//stacks in here
+class Stack {
+  constructor() {
+    this.dataStore = [];
+    this.top = 0;
+  };
+}
+
+module.exports = Stack;
--- Stacks/test/Stack.js
+++ Stacks/test/Stack.js
@@ -1,1 +1,16 @@
-//tests in here
+const expect = require('chai').expect;
+const Stack = require('../Stack.js');
+
+let myStack = new Stack();
+
+describe('Stack', function () {
+
+  it('should exist', function () {
+    expect(myStack).to.exist;
+  });
+
+  it('should be an object', function () {
+    expect(myStack).to.be.a('object');
+  });
+
+});
