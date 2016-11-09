3f2634a16e382aba06111850c82db6902505a7f2
Chad Griffis <cmg123@me.com>
2016-11-09 12:33:34 -0600

    adds testing for Stack.push function

--- Stacks/Stack.js
+++ Stacks/Stack.js
@@ -1,4 +1,5 @@
 class Stack {
+  // last in, first out (LIFO) / first in, last out (FILO)
   constructor() {
     this.dataStore = [];
     this.top = 0;
--- Stacks/test/Stack.js
+++ Stacks/test/Stack.js
@@ -13,4 +13,34 @@ describe('Stack', function () {
     expect(myStack).to.be.a('object');
   });
 
+  it('should have a dataStore property', function () {
+    expect(myStack.dataStore).to.exist;
+  });
+
+  it('should have a top property', function () {
+    expect(myStack.top).to.exist;
+  });
+
+});
+
+describe('Stack.push', function () {
+
+  it('should exist', function () {
+    expect(myStack.push).to.exist;
+  });
+
+  it('should be a function', function () {
+    expect(myStack.push).to.be.a('function');
+  });
+
+  it('should incrament the top property with each element pushed', function () {
+    myStack.push('one');
+    myStack.push('two');
+    expect(myStack.top).to.equal(2);
+  });
+
+  it('should add the element to the dataStore', function () {
+    expect(myStack.dataStore[1]).to.equal('two');
+  });
+
 });
