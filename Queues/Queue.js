25e1777b927eae413eb11fd12efc5cdcc768a144
Chad Griffis <cmg123@me.com>
2016-11-10 09:23:57 -0600

    adds testing for enqueue and dequeue and the passing functionality

--- Queues/Queue.js
+++ Queues/Queue.js
@@ -2,8 +2,15 @@ class Queue {
 
   constructor() {
     this.dataStore = [];
-  }
+  };
 
+  enqueue(element) {
+    this.dataStore.push(element);
+  };
+
+  dequeue() {
+    return this.dataStore.shift();
+  };
 }
 
 module.exports = Queue;
--- Queues/test/Queue.js
+++ Queues/test/Queue.js
@@ -18,3 +18,42 @@ describe('Queue', function () {
   });
 
 });
+
+describe('Queue.enqueue', function () {
+
+  it('should exist', function () {
+    expect(myQueue.enqueue).to.exist;
+  });
+
+  it('should be an function', function () {
+    expect(myQueue.enqueue).to.be.a('function');
+  });
+
+  it('should add an element to the end of the queue', function () {
+    myQueue.enqueue('player1');
+    myQueue.enqueue('player2');
+    myQueue.enqueue('player3');
+    expect(myQueue.dataStore[myQueue.dataStore.length - 1]).to.equal('player3');
+  });
+
+});
+
+describe('Queue.dequeue', function () {
+
+  it('should exist', function () {
+    expect(myQueue.dequeue).to.exist;
+  });
+
+  it('should be an function', function () {
+    expect(myQueue.dequeue).to.be.a('function');
+  });
+
+  it('should return the element at the front of the queue', function () {
+    expect(myQueue.dequeue()).to.equal('player1');
+  });
+
+  it('should remove the element from the front of the queue', function () {
+    expect(myQueue.dataStore[0]).to.equal('player2');
+  });
+
+});
