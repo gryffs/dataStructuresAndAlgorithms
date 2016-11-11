cf424f5de0948cc9873373bb33bbaf3f9f8e9103
Chad Griffis <cmg123@me.com>
2016-11-11 15:56:22 -0600

    adds testing for Node and adds the Node class

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,1 +1,8 @@
-//
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+};
+
+module.exports = {Node : Node};
--- LinkedList/test/LinkedList.js
+++ LinkedList/test/LinkedList.js
@@ -1,1 +1,25 @@
 const expect = require('chai').expect;
+const Node = require('../LinkedList').Node;
+const LinkedList = require('../LinkedList').LinkedList;
+
+let myNode = new Node('bacon');
+
+describe('Node', function () {
+
+  it('should exist', function () {
+    expect(myNode).to.exist;
+  });
+
+  it('should be an object', function () {
+    expect(myNode).to.be.a('object');
+  });
+
+  it('should have a element property', function () {
+    expect(myNode.element).to.exist;
+  });
+
+  it('should have a next property set to null', function () {
+    expect(myNode.next).to.equal(null);
+  });
+
+});
